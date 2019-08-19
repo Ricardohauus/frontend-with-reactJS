e575dee8bbfa9f62b2ffbc7b869b6dc78b7cf27d
Ricardo Brasil <ricardo@grupocm.net>
2019-08-19 18:15:41 -0300

    colocando o initialValues no form

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import UserList from "./pages/UserList/index";
 import UserForm from "./pages/UserForm/index";
+import UploadForm from "./pages/UploadForm/index";
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
         <Route path="/users" exact component={UserList} />
         <Route path="/users/create" component={UserForm} />
         <Route path="/users/edit/:id" component={UserForm} />
+        <Route path="/upload/create" component={UploadForm} />
       </Switch>
     </BrowserRouter>
   );
